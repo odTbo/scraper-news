3b4dbc3404b3cc4f6e4ccf8f6901b14897ad5eca
Vladimir <htcvlado@gmail.com>
2021-09-21 05:18:10 +0200

    interez

--- scrapers/constants.py
+++ scrapers/constants.py
@@ -1,6 +1,6 @@
 headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36",
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept": "text/html",
     "accept-language": "en-US;q=0.7,en;q=0.6",
 }
 
